664f5ff640954f045417cf99ec62c551c30527c8
Marco Antonio <mapi27022005@gmail.com>
2025-06-27 01:06:56 -0400

    *

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
   base: '/',
   server: {
     port: 3000,
     strictPort: true,
     open: true,
-    host: true
+    host: true,
+    fs: {
+      strict: false
+    }
   },
   publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue': ['vue']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['vue']
   }
 })
